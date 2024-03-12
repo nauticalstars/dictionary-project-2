416981160fdaef95095a0c8d2a1b5fe2e845677e
Nautical <nauticalstars21@gmail.com>
2024-03-11 20:19:31 -0700

    added Axios and dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for '${keyword}' definition`);
+    let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function keywordChange(event) {
     setKeyword(event.target.value);
