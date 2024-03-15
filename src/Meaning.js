b36af217177ccff787fdc91530baaac4006e6685
Nautical <nauticalstars21@gmail.com>
2024-03-14 19:18:02 -0700

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,11 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definition.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p className="fw-bold">
-              {definition.definition}
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <h2>{props.meaning.definition}</h2>
+      <em>{props.meaning.example}</em>
     </div>
   );
 }
