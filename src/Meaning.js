c2177b5fb8fd5dfaf8df02a569c77aba8939b1fb
Nautical <nauticalstars21@gmail.com>
2024-03-19 19:38:28 -0700

    created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <h2>{props.meaning.definition}</h2>
-      <em>{props.meaning.example}</em>
+      <h2>Definition: {props.meaning.definition}</h2>
+      <h2>Example: {props.meaning.example}</h2>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
